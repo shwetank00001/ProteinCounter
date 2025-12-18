1a7cd76e9873c9fb4cbe59f22c436945a0a17660
Shwetank <mshwetank@gmail.com>
2025-12-18 10:28:49 +0530

    Learnt text-muted-foreground (for grey texts), for (max-w-2xl so that content doesnt go out that much), leading-relaxed (for spacing btw two lines)

--- components/landing-page/HeroSection.tsx
+++ components/landing-page/HeroSection.tsx
@@ -1,14 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge"
+import Link from "next/link";
+import { SparkleIcon } from "lucide-react";
 
 const HeroSection = () => {
   return (
     <section className="bg-amber-100">
-        <div className="flex flex-col px-5 items-center justify-center">
-            <Badge variant="outline">Your gym directory here  </Badge>
-                <h1>Welcome to Shwetank's gym arena.</h1>
-                <p>You can track your gym exercises and protein goal here.</p>
-            <Button>Your everyday split</Button>
+        <div className="flex flex-col px-5 py-2 items-center justify-center">
+            <Badge className="p-2 px-4 mb-6 border-black" variant="outline"><span className="animate-ping">⭕</span>Your gym directory here  </Badge>
+                <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold max-w-5xl mb-6">Track your everyday gym progress easily.</h1>
+                <p className="text-lg sm:text-xl md:text-xl lg:text-xl text-muted-foreground mb-10 max-w-2xl leading-relaxed">A community where you can keep your splits in record. A community where you can keep your splits in record</p>
+            
+            <div className="flex flex-col sm:flex-row gap-4 mb-16 ">
+              <Button variant={"blue"} className="text-base" size="lg"><Link href={"/submit"}>🌟Add Today's split</Link></Button>
+              <Button variant={"pink"} className="text-base" size="lg"><Link href={"/splits"}>Your everyday 💪🏼split</Link></Button>
+            </div>
         </div>
     </section>
   )
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        blue: "bg-blue-400 text-white hover:bg-blue-200 hover:text-black",
+        pink: "bg-pink-400 text-white hover:bg-pink-200 hover:text-black",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -23,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        lg: "h-10 rounded-xs px-6 has-[>svg]:px-4",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
