65b3daed6eb7d4f3d664f5ca746d8e5403ca1b2e
Shwetank <mshwetank@gmail.com>
2025-12-19 15:16:28 +0530

    Status section

--- components/landing-page/DynamicStats.tsx
+++ components/landing-page/DynamicStats.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { SparkleIcon,Dumbbell  } from "lucide-react";
 
 const DynamicStats = () => {
+    const stats = [
+    {
+      icon: Dumbbell, allTimeSplits: "2K", labe: "Splits Recorded"
+    },
+    {
+      icon: Dumbbell, allTimeSplits: "2K", labe: "Splits Recorded"
+    },
+    {
+      icon: Dumbbell, allTimeSplits: "2K", labe: "Splits Recorded"
+    },
+  ]
+
+  const ele = stats.map((item) => {
+    return (
+      <div >
+        <div className="flex justify-center items-center gap-2">
+          <Dumbbell className="size-15 text-primary/70" />
+          <p className="text-3xl sm:text-4xl font-bold">{item.allTimeSplits}</p>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-sm text-muted-foreground ">{item.labe}</p>
+        </div>
+      </div>
+    )
+  })
   return (
-    <div>DynamicStats</div>
+    <div className="grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-4xl items-center w-full"  >
+      {ele}
+    </div>
   )
 }
 
--- components/landing-page/HeroSection.tsx
+++ components/landing-page/HeroSection.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge"
 import Link from "next/link";
-import { SparkleIcon } from "lucide-react";
+import DynamicStats from "./DynamicStats";
 
 const HeroSection = () => {
+
+
   return (
     <section className="bg-amber-100">
         <div className="flex flex-col px-5 py-2 items-center justify-center">
@@ -15,6 +17,11 @@ const HeroSection = () => {
               <Button variant={"blue"} className="text-base" size="lg"><Link href={"/submit"}>🌟Add Today's split</Link></Button>
               <Button variant={"pink"} className="text-base" size="lg"><Link href={"/splits"}>Your everyday 💪🏼split</Link></Button>
             </div>
+        </div>
+
+
+        <div className="flex justify-center" >
+          <DynamicStats />  
         </div>
     </section>
   )
