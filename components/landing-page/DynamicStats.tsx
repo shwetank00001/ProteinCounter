951aa4b9b30f0bb3ed096275d011c51f6ae871b3
shwetank00001 <mshwetank@gmail.com>
2026-01-01 17:11:56 +0530

    Working from todayyyy

--- components/landing-page/DynamicStats.tsx
+++ components/landing-page/DynamicStats.tsx
@@ -3,13 +3,13 @@ import { SparkleIcon,Dumbbell  } from "lucide-react";
 const DynamicStats = () => {
     const stats = [
     {
-      icon: Dumbbell, allTimeSplits: "2K", labe: "Splits Recorded"
+      icon: Dumbbell, allTimeSplits: "2K", label: "Splits Recorded"
     },
     {
-      icon: Dumbbell, allTimeSplits: "2K", labe: "Splits Recorded"
+      icon: Dumbbell, allTimeSplits: "10K+", label: "Protein Saved"
     },
     {
-      icon: Dumbbell, allTimeSplits: "2K", labe: "Splits Recorded"
+      icon: Dumbbell, allTimeSplits: "2K", label: "Splits Recorded"
     },
   ]
 
@@ -21,7 +21,7 @@ const DynamicStats = () => {
           <p className="text-3xl sm:text-4xl font-bold">{item.allTimeSplits}</p>
         </div>
         <div className="flex justify-center">
-          <p className="text-sm text-muted-foreground ">{item.labe}</p>
+          <p className="text-sm text-muted-foreground ">{item.label}</p>
         </div>
       </div>
     )
